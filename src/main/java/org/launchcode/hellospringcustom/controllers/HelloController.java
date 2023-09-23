0f5d9694ecd5ed09f7aafa81a141d37430df523a
Julia Loftin <julia.loftin7@gmail.com>
2023-09-23 12:08:16 -0500

    completes controller exercises

--- src/main/java/org/launchcode/hellospringcustom/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospringcustom/controllers/HelloController.java
@@ -19,19 +19,49 @@ public class HelloController {
     }
 
     // Handles requests to the form /hello?name=LaunchCode
-    @RequestMapping(method = { RequestMethod.GET, RequestMethod.POST }, value= "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
+//    @RequestMapping(method = { RequestMethod.GET, RequestMethod.POST }, value= "hello")
+//    @ResponseBody
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
 
     // Handles requests to the form /hello/LaunchCode
-    @GetMapping("hello/{name}")
+    @RequestMapping(value="hello", method = RequestMethod.POST)
     @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloPost(@RequestParam String name, @RequestParam String langs) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, langs);
+
+        // For a bonus mission, students can change this response text to look nicer.
+        // This is subjective, but students should be modifying the HTML of the response string.
     }
 
+    public static String createMessage(String name, String langs) {
+        String greeting = "";
+
+        if (langs.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (langs.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (langs.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (langs.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (langs.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + name;
+    }
+    // Modify your HelloController class to display a form on a GET request that asks the user for both their name and
+    // the language they would like to be greeted in.
     @GetMapping("form")
     @ResponseBody
     public String helloForm() {
@@ -39,10 +69,19 @@ public class HelloController {
                     "<body>" +
                         "<form action='hello' method='post'>" +
                             "<input type='text' name='name' />" +
+                            "<select name='langs' id='lang-select'>" +
+                                "<option value='english'>English</option>" +
+                                "<option value='french'>French</option>" +
+                                "<option value='italian'>Italian</option>" +
+                                "<option value='spanish'>Spanish</option>" +
+                                "<option value='german'>German</option>" +
+                            "<select/>" +
                             "<input type='submit' value='Greet me!' />" +
                         "</form>" +
                     "</body>" +
                 "</html>";
     }
+
+
 
 }
