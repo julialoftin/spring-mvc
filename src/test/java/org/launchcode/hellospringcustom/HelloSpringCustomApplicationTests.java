c08a2ebb85672388a84dac6534f4baca89b89c85
Julia Loftin <julia.loftin7@gmail.com>
2023-09-22 21:05:36 -0500

    completes controller examples

---
+++ src/test/java/org/launchcode/hellospringcustom/HelloSpringCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringcustom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringCustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
